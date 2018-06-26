e1281f8f2a7155316e0ac4b3fbcb0a1001d8cf66
riyenas0925 <riyenas0925@gmail.com>
2018-06-26 14:59:00 +0900

    테스트

--- SW_track/firstServer/track/views.py
+++ SW_track/firstServer/track/views.py
@@ -17,19 +17,17 @@ def keyboard(request):
 
 
 @csrf_exempt
-def message(request) :
+def message(request):
     message = ((request.body).decode('utf-8'))
     return_json_str = json.loads(message)
     return_str = return_json_str['content']
- 
-    requestMode = return_str.encode('utf-8') # utf-8형식으로 인코딩하여 한글을 인식
-     
-    if requestMode == '트랙조회' :
-        return JsonResponse({
-             'message' : {
-                 'text' : "번역할 내용을 다음과 같이 입력해 주세요.(개발중)\n ex)번역 뭐해?, 번역 안녕ㅋㅋ\n 형식을 갖추지 않으면 답변이 나오지 않습니다ㅜㅜ."
-             },
-             'keyboard' : {
-                 'type' : 'text' # 텍스트로 입력받기 위하여 키보드 타입을 text로 설정
-             }
-        })
+
+    return JsonResponse({
+            'message': {
+                    'text': "button test : " + return_str
+            },
+            'keyboard': {
+                    'type': 'buttons',
+                    'buttons': ['1','2']
+            }
+    })
