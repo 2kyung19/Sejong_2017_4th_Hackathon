d8a86d6e8872d0fddbfb2d3b97a838369053eecc
riyenas0925 <riyenas0925@gmail.com>
2018-06-26 14:26:51 +0900

    수정

--- SW_track/firstServer/track/views.py
+++ SW_track/firstServer/track/views.py
@@ -7,3 +7,12 @@ def keyboard(request):
         'type' : 'buttons',
         'buttons' : ['상록원', '그루터기', '아리수', '기숙사식당', '교직원식당']
     })
+
+
+@csrf_exempt
+def message(request):
+    message = ((request.body).decode('utf-8'))
+    return_json_str = json.loads(message)
+    return_str = return_json_str['content'] #버튼 항목중 무엇을 눌렀는가
+
+    return return_str
