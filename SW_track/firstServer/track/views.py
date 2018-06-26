58ef545d4ba9f4e1b6a604d983a2d1d74ff7ccc2
riyenas0925 <riyenas0925@gmail.com>
2018-06-26 14:47:05 +0900

    수정

--- SW_track/firstServer/track/views.py
+++ SW_track/firstServer/track/views.py
@@ -12,18 +12,5 @@ def keyboard(request):
 
     return JsonResponse({
         'type' : 'buttons',
-        'buttons' : ['상록원', '그루터기', '아리수', '기숙사식당', '교직원식당']
-    })
-
-
-@csrf_exempt
-def message(request):
-    message = ((request.body).decode('utf-8'))
-    return_json_str = json.loads(message)
-    return_str = return_json_str['content'] #버튼 항목중 무엇을 눌렀는가
-
-    return JsonResponse({ #return 밑에는 공통어
-        "message": {
-            "text": '٩(๑`^´๑)۶\n키워드를 넣어서 물어보라냥~!\n\n-키워드-\n*학생회관\n*군자관\n*우정당\nex)학생회관,군자관 목\n\n -키워드-\n*미세먼지\n*날씨\nex)오늘 날씨 어때?,날씨\n\n-키워드-\n*공지사항\n*개발자'
-        }
+        'buttons' : ['트랙조회', '1', '아리수', '기숙사식당', '교직원식당']
     })
