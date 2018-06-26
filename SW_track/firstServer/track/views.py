04bf73940867aa8c44555527d720a64efa8069f5
2kyung19 <32586712+2kyung19@users.noreply.github.com>
2018-06-26 15:36:23 +0900

    test

--- SW_track/firstServer/track/views.py
+++ SW_track/firstServer/track/views.py
@@ -12,7 +12,8 @@ import requests
 def keyboard(request):
     return JsonResponse(
         {
-            "type" : "text"
+            "type" : "buttons"
+            "buttons" : ["트랙 조회","전체 트랙 보기"]
         }   
     )
 
@@ -21,9 +22,13 @@ def message(request):
     message = ((request.body).decode('utf-8'))
     return_json_str = json.loads(message)
     return_str = return_json_str['content'] #버튼 항목중 무엇을 눌렀는가
+
     
     return JsonResponse({ #return 밑에는 공통어
         "message": {
-            "text": '٩(๑`^´๑)۶\n키워드를 넣어서 물어보라냥~!\n\n-키워드-\n*학생회관\n*군자관\n*우정당\nex)학생회관,군자관 목\n\n -키워드-\n*미세먼지\n*날씨\nex)오늘 날씨 어때?,날씨\n\n-키워드-\n*공지사항\n*개발자'
+            "text": '학번을 입력해주세요.\nex)17010491'
+        },
+        'keyboard':{
+            'type':'text'
         }
     })
