11cbdb09a11d46f8fa8c2daa90d4a4605966ee52
riyenas0925 <riyenas0925@gmail.com>
2018-06-27 02:06:21 +0900

    test

--- SW_track/firstServer/track/models.py
+++ SW_track/firstServer/track/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Track(models.Model):
     student_id = models.IntegerField(default=1)
-    name = models.TextField(max_length=30)
-    base = models.TextField(max_length=30)
-    use = models.TextField(max_length=30)
+    name = models.TextField(max_length=256)
+    base = models.TextField(max_length=256)
+    use = models.TextField(max_length=256)
