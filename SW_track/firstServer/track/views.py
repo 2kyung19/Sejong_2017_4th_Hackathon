411a2ffb4314753e7916ac9f68de934fa4fb24f0
riyenas0925 <riyenas0925@gmail.com>
2018-06-26 14:54:54 +0900

    테스트

--- SW_track/firstServer/track/views.py
+++ SW_track/firstServer/track/views.py
@@ -17,13 +17,19 @@ def keyboard(request):
 
 
 @csrf_exempt
-def message(request):
+def message(request) :
     message = ((request.body).decode('utf-8'))
     return_json_str = json.loads(message)
-    return_str = return_json_str['content'] #버튼 항목중 무엇을 눌렀는가
-
-    return JsonResponse({ #return 밑에는 공통어
-        "message": {
-            "text": '٩(๑`^´๑)۶\n키워드를 넣어서 물어보라냥~!\n\n-키워드-\n*학생회관\n*군자관\n*우정당\nex)학생회관,군자관 목\n\n -키워드-\n*미세먼지\n*날씨\nex)오늘 날씨 어때?,날씨\n\n-키워드-\n*공지사항\n*개발자'
-        }
-    })
+    return_str = return_json_str['content']
+ 
+    requestMode = return_str.encode('utf-8') # utf-8형식으로 인코딩하여 한글을 인식
+     
+    if requestMode == '트랙조회' :
+        return JsonResponse({
+             'message' : {
+                 'text' : "번역할 내용을 다음과 같이 입력해 주세요.(개발중)\n ex)번역 뭐해?, 번역 안녕ㅋㅋ\n 형식을 갖추지 않으면 답변이 나오지 않습니다ㅜㅜ."
+             },
+             'keyboard' : {
+                 'type' : 'text' # 텍스트로 입력받기 위하여 키보드 타입을 text로 설정
+             }
+        })
